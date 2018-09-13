b4c3fbbecba2a370a299b5e220c4afb7d61e1742
jackcutri <42977923+jackcutri@users.noreply.github.com>
2018-09-12 21:14:04 -0400

    Add files via upload

--- routes.py
+++ routes.py
@@ -11,24 +11,10 @@ def index():
 @app.route('/')
 @app.route('/artists')
 def artists():
-    artist =[
-        {
-            'artist': {'name': 'King Krule'},
-            'bio': 'Songwriter from the underground scene who combines jazz with elements of hip-hop and punk music.',
-            'hometown': 'London, England',
-            'upcomingEvents': 'The Haunt- 9/15/18 @ 9:00pm'
-        },
-        {
-            'artist': {'name': 'Jimi Hendrix'},
-            'bio': 'Psychedelic blues guitarist known for his explosive live performances in the 60s.',
-            'hometown': 'Seattle, Washington',
-            'upcomingEvents': 'none'
-        },
-        {
-            'artist': {'name': 'John Frusciante'},
-            'bio': 'Ex-guitarist of the Red Hot Chili Peppers.',
-            'hometown': 'Queens, New York',
-            'upcomingEvents': 'The Range- 10/10/18 @ 10:00pm'
-        }
-    ]
-    return render_template('artists.html', title='List of Artists', artist=artist)
+    artist_list = ["Jimi Hendrix", "John Frusciante", "King Krule"]
+    return render_template('artists.html', title='List of Artists', artists=artist_list)
+
+@app.route('/')
+@app.route('/specificArtist')
+def specific():
+    return render_template('specificArtist.hmtl')
