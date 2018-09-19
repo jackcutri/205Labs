4e74b1badf869b8a63142b9499630485a3601ba6
jackcutri <42977923+jackcutri@users.noreply.github.com>
2018-09-18 20:01:04 -0400

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import NewArtistForm
 
 
 @app.route('/')
@@ -8,11 +9,13 @@ def index():
     user = {'username': 'Jack'}
     return render_template('index.html', title='Home', user=user)
 
+
 @app.route('/')
 @app.route('/artists')
 def artists():
     artist_list = ["Jimi Hendrix", "John Frusciante", "King Krule"]
     return render_template('artists.html', title='List of Artists', artists=artist_list)
+
 
 @app.route('/')
 @app.route('/specificArtist')
@@ -25,3 +28,17 @@ def specific():
 
     return render_template('specificArtist.html', info=info)
 
+
+@app.route('/newArtists', methods=['GET', 'POST'])
+def create():
+    form = NewArtistForm()
+
+    if form.validate_on_submit():
+        flash('Thank you, a new artist has been created.')
+        info = {}
+        info["name"] = form.name.data
+        info["hometown"] = form.hometown.data
+        info["bio"] = form.description.data
+
+        return render_template('/specificArtist.html', info=info)
+    return render_template('newArtists.html', title='Create New Artist', form=form)
